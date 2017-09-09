ed0bc8935808570f5ddf074a604a14ecbe0fe5b4
Solra Bizna <solra@bizna.name>
2017-09-09 00:49:37 -0600

    We don't need the alternating colorburst anymore, yay

--- main.js
+++ main.js
@@ -12,8 +12,7 @@ try {
     let NUM_PHASES = 12;
     let OUT_RATIO = 2;
     let INTERESTING_ELEMENTS = [
-        "image_selector","prescale_checkbox","display","still_checkbox",
-        "still_div","still_image","still_selector","scanline_checkbox",
+        "image_selector","prescale_checkbox","display","scanline_checkbox",
         "progress"
     ];
     let el = {};
@@ -31,14 +30,12 @@ try {
     // quick stop to make HYBRID_FIR more memory efficient
     assume("Float32Array");
     HYBRID_FIR.kernel.forEach(function(phase) {
-        phase.forEach(function(subpixel) {
-            let MULT = 1/65536;
-            for(let yoff = 0; subpixel[yoff]; ++yoff) {
-                let old = subpixel[yoff];
-                for(let n = 0; n < old.length; ++n) old[n] *= MULT;
-                subpixel[yoff] = Float32Array.from(old);
-            }
-        });
+        let MULT = 1/65536;
+        for(let subpixel = 0; phase[subpixel]; ++subpixel) {
+            let old = phase[subpixel];
+            for(let n = 0; n < old.length; ++n) old[n] *= MULT;
+            phase[subpixel] = Float32Array.from(old);
+        }
     });
     // a slapdash pipeline for image processing! HOORAY!
     assume("Image");
@@ -118,10 +115,9 @@ try {
     }
     // processing logic
     {
-        let NUM_IMAGES = 3;
         let procPixels, outPixels;
-        let images, imagedatas, imagebitmaps; // even, odd, average
-        let curImageNo, curImageRow;
+        let image;
+        let curImageRow;
         let active = false;
         let proc;
         let newOutPixels = function() {
@@ -130,202 +126,122 @@ try {
         beginNewProc = function(newProcPixels) {
             procPixels = newProcPixels;
             outPixels = newOutPixels();
-            images = [];
-            imagedatas = [];
-            imagebitmaps = [];
-            curImageNo = 0;
+            image = null;
             curImageRow = 0;
             if(!active) {
                 active = true;
                 window.requestAnimationFrame(proc);
             }
         }
-        let updateStill = function() {
-            if(el.still_selector.selectedIndex < curImageNo) {
-                el.still_image.src = images[el.still_selector.selectedIndex].src;
-            }
-        }
         proc = function(now) {
             let deadline = now + 60;
-            while(curImageNo < NUM_IMAGES && performance.now() < deadline) {
-                while(curImageRow < outPixels.height
-                      && performance.now() < deadline) {
-                    if(curImageRow < procPixels.height) {
-                        // the actual filter
-                        let outp = outPixels.width * 4 * (curImageRow*2);
-                        if(curImageNo == 2) {
-                            for(let out_x = 0; out_x < outPixels.width; ++out_x) {
-                                outPixels.data[outp] = (imagedatas[0].data[outp] + imagedatas[1].data[outp]) / 2;
-                                ++outp;
-                                outPixels.data[outp] = (imagedatas[0].data[outp] + imagedatas[1].data[outp]) / 2;
-                                ++outp;
-                                outPixels.data[outp] = (imagedatas[0].data[outp] + imagedatas[1].data[outp]) / 2;
-                                ++outp;
-                                outPixels.data[outp++] = 255;
-                            }
+            while(curImageRow < outPixels.height
+                  && performance.now() < deadline) {
+                if(curImageRow < procPixels.height) {
+                    // the actual filter
+                    let outp = outPixels.width * 4 * (curImageRow*2);
+                    for(let out_x = 0; out_x < outPixels.width; ++out_x) {
+                        let center_x = Math.floor(out_x / OUT_RATIO);
+                        let start_filtp = 0;
+                        let start_x = center_x - HYBRID_FIR.radius - 1;
+                        let start_pixp = start_x * 4;
+                        if(start_x < 0) {
+                            start_pixp = start_pixp + (start_x * -4);
+                            start_filtp = start_filtp + (start_x * -9);
+                            start_x = 0;
                         }
-                        else {
-                            for(let out_x = 0; out_x < outPixels.width; ++out_x) {
-                                let center_x = Math.floor(out_x / OUT_RATIO);
-                                let start_filtp = 0;
-                                let start_x = center_x - HYBRID_FIR.radius;
-                                let start_pixp = start_x * 4;
-                                if(start_x < 0) {
-                                    start_pixp = start_pixp + (start_x * -4);
-                                    start_filtp = start_filtp + (start_x * -9);
-                                    start_x = 0;
-                                }
-                                let end_x = center_x + HYBRID_FIR.radius;
-                                if(end_x >= procPixels.width)
-                                    end_x = procPixels.width - 1;
-                                let phase = (67 + center_x) % NUM_PHASES;
-                                if(curImageRow & 1)
-                                    phase = (phase + NUM_PHASES/2) % NUM_PHASES;
-                                let subpixel = out_x % OUT_RATIO;
-                                let r = 0, g = 0, b = 0;
-                                let go = function() {
-                                    for(let yoff = -1; yoff <= 1; ++yoff) {
-                                        let in_y = curImageRow + yoff;
-                                        if(in_y < 0) continue;
-                                        else if(in_y >= procPixels.height) break;
-                                        let inpixp = in_y * procPixels.width * 4 + start_pixp;
-                                        let infiltp = start_filtp;
-                                        let local_phase;
-                                        let filt = HYBRID_FIR.kernel[phase][subpixel][yoff+1];
-                                        for(let in_x = start_x; in_x <= end_x; ++in_x) {
-                                            let in_r = procPixels.data[inpixp++];
-                                            let in_g = procPixels.data[inpixp++];
-                                            let in_b = procPixels.data[inpixp++];
-                                            ++inpixp;
-                                            r += in_r * filt[infiltp++];
-                                            g += in_r * filt[infiltp++];
-                                            b += in_r * filt[infiltp++];
-                                            r += in_g * filt[infiltp++];
-                                            g += in_g * filt[infiltp++];
-                                            b += in_g * filt[infiltp++];
-                                            r += in_b * filt[infiltp++];
-                                            g += in_b * filt[infiltp++];
-                                            b += in_b * filt[infiltp++];
-                                        }
-                                    }
-                                };
-                                switch(curImageNo) {
-                                case 1:
-                                    phase = (phase + NUM_PHASES/2) % NUM_PHASES;
-                                case 0:
-                                    go();
-                                    break;
-                                case 2:
-                                    go();
-                                    phase = (phase + NUM_PHASES/2) % NUM_PHASES;
-                                    go();
-                                    r *= 0.5; g *= 0.5; b *= 0.5;
-                                    break;
-                                }
-                                outPixels.data[outp++] = r;
-                                outPixels.data[outp++] = g;
-                                outPixels.data[outp++] = b;
-                                outPixels.data[outp++] = 255;
-                            }
+                        let end_x = center_x + HYBRID_FIR.radius - 1;
+                        if(end_x >= procPixels.width)
+                            end_x = procPixels.width - 1;
+                        let phase = (67 + center_x) % NUM_PHASES;
+                        if(curImageRow & 1)
+                            phase = (phase + NUM_PHASES/2) % NUM_PHASES;
+                        let subpixel = out_x % OUT_RATIO;
+                        let r = 0, g = 0, b = 0;
+                        let inpixp = curImageRow * procPixels.width * 4 + start_pixp;
+                        let infiltp = start_filtp;
+                        let local_phase;
+                        let filt = HYBRID_FIR.kernel[phase][subpixel];
+                        for(let in_x = start_x; in_x <= end_x; ++in_x) {
+                            let in_r = procPixels.data[inpixp++];
+                            let in_g = procPixels.data[inpixp++];
+                            let in_b = procPixels.data[inpixp++];
+                            ++inpixp;
+                            r += in_r * filt[infiltp++];
+                            g += in_r * filt[infiltp++];
+                            b += in_r * filt[infiltp++];
+                            r += in_g * filt[infiltp++];
+                            g += in_g * filt[infiltp++];
+                            b += in_g * filt[infiltp++];
+                            r += in_b * filt[infiltp++];
+                            g += in_b * filt[infiltp++];
+                            b += in_b * filt[infiltp++];
                         }
+                        outPixels.data[outp++] = r;
+                        outPixels.data[outp++] = g;
+                        outPixels.data[outp++] = b;
+                        outPixels.data[outp++] = 255;
                     }
-                    else if(el.scanline_checkbox.checked) {
-                        // interpolate scanlines
-                        let inp1 = outPixels.width * 4 * (curImageRow-procPixels.height)*2;
-                        let outp = inp1 + outPixels.width * 4;
-                        let inp2 = outp + outPixels.width * 4;
-                        for(let x = 0; x < outPixels.width; ++x) {
-                            let ra = Math.pow(outPixels.data[inp1++],2.2);
-                            let ga = Math.pow(outPixels.data[inp1++],2.2);
-                            let ba = Math.pow(outPixels.data[inp1++],2.2);
-                            inp1++;
-                            let rb = Math.pow(outPixels.data[inp2++],2.2);
-                            let gb = Math.pow(outPixels.data[inp2++],2.2);
-                            let bb = Math.pow(outPixels.data[inp2++],2.2);
-                            inp2++;
-                            outPixels.data[outp++] = Math.pow((ra+rb)/8,1/2.2);
-                            outPixels.data[outp++] = Math.pow((ga+gb)/8,1/2.2);
-                            outPixels.data[outp++] = Math.pow((ba+bb)/8,1/2.2);
-                            outPixels.data[outp++] = 255;
-                        }
-                    }
-                    else {
-                        // duplicate scanlines
-                        let inp = outPixels.width * 4 * (curImageRow-procPixels.height)*2;
-                        let outp = inp + outPixels.width * 4;
-                        for(let x = 0; x < outPixels.width; ++x) {
-                            outPixels.data[outp++] = outPixels.data[inp++];
-                            outPixels.data[outp++] = outPixels.data[inp++];
-                            outPixels.data[outp++] = outPixels.data[inp++];
-                            outPixels.data[outp++] = outPixels.data[inp++];
-                        }
-                    }
-                    ++curImageRow;
                 }
+                else if(el.scanline_checkbox.checked) {
+                    // interpolate scanlines
+                    let inp1 = outPixels.width * 4 * (curImageRow-procPixels.height)*2;
+                    let outp = inp1 + outPixels.width * 4;
+                    let inp2 = outp + outPixels.width * 4;
+                    for(let x = 0; x < outPixels.width; ++x) {
+                        let ra = Math.pow(outPixels.data[inp1++],2.2);
+                        let ga = Math.pow(outPixels.data[inp1++],2.2);
+                        let ba = Math.pow(outPixels.data[inp1++],2.2);
+                        inp1++;
+                        let rb = Math.pow(outPixels.data[inp2++],2.2);
+                        let gb = Math.pow(outPixels.data[inp2++],2.2);
+                        let bb = Math.pow(outPixels.data[inp2++],2.2);
+                        inp2++;
+                        outPixels.data[outp++] = Math.pow((ra+rb)/8,1/2.2);
+                        outPixels.data[outp++] = Math.pow((ga+gb)/8,1/2.2);
+                        outPixels.data[outp++] = Math.pow((ba+bb)/8,1/2.2);
+                        outPixels.data[outp++] = 255;
+                    }
+                }
+                else {
+                    // duplicate scanlines
+                    let inp = outPixels.width * 4 * (curImageRow-procPixels.height)*2;
+                    let outp = inp + outPixels.width * 4;
+                    for(let x = 0; x < outPixels.width; ++x) {
+                        outPixels.data[outp++] = outPixels.data[inp++];
+                        outPixels.data[outp++] = outPixels.data[inp++];
+                        outPixels.data[outp++] = outPixels.data[inp++];
+                        outPixels.data[outp++] = outPixels.data[inp++];
+                    }
+                }
+                ++curImageRow;
                 if(curImageRow >= outPixels.height) {
                     canvas.width = outPixels.width;
                     canvas.height = outPixels.height;
                     let ctx = canvas.getContext("2d");
                     ctx.putImageData(outPixels, 0, 0);
-                    images[curImageNo] = new Image();
-                    images[curImageNo].src = canvas.toDataURL("image/png");
-                    imagedatas[curImageNo] = outPixels;
-                    if("createImageBitmap" in window) {
-                        let cpp = procPixels;
-                        let cin = curImageNo;
-                        createImageBitmap(outPixels).then(
-                            function(response) {
-                                if(procPixels == cpp) {
-                                    imagebitmaps[cin] = response;
-                                }
-                            }
-                        );
-                    }
-                    ++curImageNo;
-                    outPixels = newOutPixels();
-                    curImageRow = 0;
-                    updateStill();
+                    image = new Image();
+                    image.src = canvas.toDataURL("image/png");
+                    active = false;
+                    progress.style.display = "none";
+                    el.display.width = outPixels.width;
+                    el.display.height = outPixels.height;
+                    el.display.src = image.src;
+                    el.display.style.display = "block";
                 }
-            }
-            if(curImageNo >= NUM_IMAGES) {
-                active = false;
-                progress.style.display = "none";
-            }
-            else {
-                progress.max = NUM_IMAGES * outPixels.height;
-                progress.value = curImageNo * outPixels.height + curImageRow;
-                progress.style.display = "block";
+                else {
+                    progress.max = outPixels.height;
+                    progress.value = curImageRow;
+                    progress.style.display = "block";
+                    el.display.style.display = "none";
+                }
             }
             if(active)
                 window.requestAnimationFrame(proc);
         }
-        let drawn = 0;
-        let draw = function() {
-            if(curImageNo >= 2) {
-                el.display.style.display = "block";
-                el.display.width = imagedatas[drawn].width;
-                el.display.height = imagedatas[drawn].height;
-                let ctx = el.display.getContext("2d");
-                if(imagebitmaps[drawn])
-                    ctx.drawImage(imagebitmaps[drawn], 0, 0);
-                else
-                    ctx.putImageData(imagedatas[drawn], 0, 0);
-                if(++drawn >= 2) drawn = 0;
-            }
-            else {
-                el.display.style.display = "none";
-            }
-            window.requestAnimationFrame(draw);
-        }
-        window.requestAnimationFrame(draw);
         el.scanline_checkbox.onchange = function() {
             if(procPixels !== undefined)
                 beginNewProc(procPixels);
-        }
-        el.still_selector.onchange = updateStill;
-        el.still_checkbox.onchange = function() {
-            el.still_div.style.display = el.still_checkbox.checked
-                ? "block" : "none";
         }
     }
 }
